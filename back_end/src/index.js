871d13cc8a56c61d15639725a8f7795d6c72d393
hungthehunter <133243197+hungthehunter@users.noreply.github.com>
2025-05-01 15:20:46 +0700

    fix up to web

--- back_end/src/index.js
+++ back_end/src/index.js
@@ -10,7 +10,11 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://fantastyburger.vercel.app', 
+  credentials: true, 
+}));
+
 app.use(bodyParser.json());
 app.use(cookieParser())
 
